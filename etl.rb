09cb2037e64b5c44743b314dbcb245e276a988b3
c1505 <coreym247@gmail.com>
2016-08-15 03:25:56 -0400

    updated naming

--- etl.rb
+++ etl.rb
@@ -1,8 +1,8 @@
 class ETL
   def self.transform(old)
-    old.each_with_object({}) do |(k, v), new_hash|
-      v.each do |arg|
-        new_hash[arg.downcase] = k
+    old.each_with_object({}) do |(score, letters), score_per_letter|
+      letters.each do |letter|
+        score_per_letter[letter.downcase] = score
       end
     end
   end
