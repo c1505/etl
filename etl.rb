44eb3c4868aee5ccf334adaad133426401f825b4
c1505 <coreym247@gmail.com>
2016-08-15 03:13:39 -0400

    refactored using each_with_object

--- etl.rb
+++ etl.rb
@@ -1,7 +1,7 @@
 class ETL
   def self.transform(old)
-    new_hash = {} #each with object instead?
-    old.each do |k, v|
+    # new_hash = {} #each with object instead?
+    old.each_with_object({}) do |(k, v), new_hash|
       v.each do |arg|
         new_hash[arg.downcase] = k
       end
