02e660b2359a957644e428b99044758ef5764797
c1505 <coreym247@gmail.com>
2016-08-15 03:20:57 -0400

    fixed error

--- etl.rb
+++ etl.rb
@@ -1,11 +1,9 @@
 class ETL
   def self.transform(old)
-    # new_hash = {} #each with object instead?
     old.each_with_object({}) do |(k, v), new_hash|
       v.each do |arg|
         new_hash[arg.downcase] = k
       end
     end
-    new_hash
   end
 end
